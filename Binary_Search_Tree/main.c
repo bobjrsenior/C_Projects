30f610d65f3996955c5df18e10e5e7074e13e2ea
Bobjrsenior <bobjrsenior@gmail.com>
2015-10-08 15:21:08 +0000

    Started work on making tests. CUrrently the findMin, findMinValue, findMinNoRec, and findMinValueNoRec methods are tested

--- Binary_Search_Tree/main.c
+++ Binary_Search_Tree/main.c
@@ -19,6 +19,8 @@
 #include <stdio.h>
 #include "Binary_Search_Tree.h"
 
+void minTests(Node* root);
+
 int main(){
 	Node* root = createNewNodeValue(13);
 	addNode(root, createNewNodeValue(8));
@@ -27,6 +29,43 @@ int main(){
 	addNode(root, createNewNodeValue(18));
 	addNode(root, createNewNodeValue(21));
 	
-	printf("  %d\n %d %d\n%d %d  %d\n", root->value, root->leftChild->value, root->rightChild->value, root->leftChild->leftChild->value, root->leftChild->rightChild->value, root->rightChild->rightChild->value);
+	minTests(root);	
+
+//	printf("  %d\n %d %d\n%d %d  %d\n", root->value, root->leftChild->value, root->rightChild->value, root->leftChild->leftChild->value, root->leftChild->rightChild->value, root->rightChild->rightChild->value);
 	return 0;
 }
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  minTests
+ *  Description: Tests the BST functions for finding a minimum value/Node 
+ * =====================================================================================
+ */
+void minTests ( Node* root ){
+	Node* minNode = findMin(root);
+	if(minNode->value != 4){
+		printf("FAILED: findMin() test\n");
+	}else{
+		printf("PASSED: findMin() test\n");
+	}
+	minNode = findMinNoRec(root);
+	if(minNode->value != 4){
+		printf("FAILED: findMinNoRec() test\n");
+	}else{
+		printf("PASSED: findMinNoRec() test\n");
+	}
+
+	int minValue = findMinValue(root);
+	if(minValue != 4){
+		printf("FAILED: findMinValue() test\n");
+	}else{
+		printf("PASSED: findMinValue() test\n");
+	}
+	minValue = findMinValueNoRec(root);
+	if(minValue != 4){
+		printf("FAILED: findMinValueNoRec() test\n");
+	}else{
+		printf("PASSED: findMinValueNoRec() test\n");
+	}
+}/* -----  end of function minTests  ----- */
